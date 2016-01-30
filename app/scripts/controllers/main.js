7bdc5d33b071fe448fbcb8d6e91ae23dc55fcdb3
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-01-30 12:54:29 +0100

    primeras pruebas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,11 +11,26 @@ angular.module('smcApp')
 
   .controller('MainCtrl', function ($scope) {
 
-    var mySplitText = $("#quote").splitText({'type':'words','animation':'glowOnHover','useLite':true});
-    var wordsElement = $(".word-measure");
-    _.each(wordsElement, function(element){
-      TweenMax.from(element, Math.floor((Math.random() * 7) + 1), {opacity:0, scale:Math.floor((Math.random() * 5) + 0), y: Math.floor((Math.random() * 100) + 0), transformOrigin:"0% 50% -50",  delay: Math.floor((Math.random() * 3) + 0),ease: Elastic.easeOut}, Math.random());
-    })
+    $(window).bind('mousewheel DOMMouseScroll', function(event){
+      if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+        console.log('ruleta PArriba')
+      }
+      else {
+        console.log('ruleta PAbajo')
+      }
+    });
+
+    function animateText(){
+      var mySplitText = $("#quote").splitText({'type':'words','animation':'glowOnHover','useLite':true});
+      var wordsElement = $(".word-measure");
+      _.each(wordsElement, function(element){
+        TweenMax.from(element, Math.floor((Math.random() * 7) + 1), {opacity:0, scale:Math.floor((Math.random() * 5) + 0), y: Math.floor((Math.random() * 100) + 0), transformOrigin:"0% 50% -50",  delay: Math.floor((Math.random() * 3) + 0),ease: Elastic.easeOut}, Math.random());
+      })
+    }
+
+    animateText();
+
+    var tl = new TimelineMax();
 
     TweenMax.from(".napFace", 1, {opacity: 0, scale: 0, delay: 0.5, ease: Elastic.easeOut});
 
@@ -31,8 +46,12 @@ angular.module('smcApp')
       TweenMax.to(".napFace", 0.5, {rotation: value, ease: Elastic.easeOut});
     }
 
-    setInterval(function(){
-      TweenMax.to(".napFace", 0.5, {opacity: 0, delay: 2, ease: Elastic.easeOut, onComplete: complete});}, 8000);
+    TweenMax.to(".napFace", 0.5, {opacity: 0, repeatDelay: 8, ease: Elastic.easeOut, onComplete: complete});
+
+    /*setInterval(function(){
+      animateText();
+      TweenMax.to(".napFace", 0.5, {opacity: 0, delay: 2, ease: Elastic.easeOut, onComplete: complete});
+    }, 8000);*/
 
     var items = $('.boxAnim');
 
