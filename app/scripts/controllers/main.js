77f0af1e4dd38edd1b96923dd08658b7146fe767
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-01-30 15:48:13 +0100

    mouse wheel events

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,47 +11,50 @@ angular.module('smcApp')
 
   .controller('MainCtrl', function ($scope) {
 
+    TweenMax.set(".napFace, .boxAnim, .word-measure", {visibility:"visible"})
+
+    var tl = new TimelineMax();
+
     $(window).bind('mousewheel DOMMouseScroll', function(event){
+      event.preventDefault();
       if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-        console.log('ruleta PArriba')
+        tl.play();
+        setTimeout(pauseAnim, 500)
       }
-      else {
-        console.log('ruleta PAbajo')
+      else if(event.originalEvent.wheelDelta < 0 || event.originalEvent.detail < 0){
+        tl.reverse();
+        setTimeout(pauseAnim, 500)
       }
+      else tl.pause();
     });
+
+    function pauseAnim(){
+      tl.pause();
+    }
 
     function animateText(){
       var mySplitText = $("#quote").splitText({'type':'words','animation':'glowOnHover','useLite':true});
       var wordsElement = $(".word-measure");
       _.each(wordsElement, function(element){
-        TweenMax.from(element, Math.floor((Math.random() * 7) + 1), {opacity:0, scale:Math.floor((Math.random() * 5) + 0), y: Math.floor((Math.random() * 100) + 0), transformOrigin:"0% 50% -50",  delay: Math.floor((Math.random() * 3) + 0),ease: Elastic.easeOut}, Math.random());
+        tl.from(element, Math.floor((Math.random() * 7) + 1), {opacity:0, scale:Math.floor((Math.random() * 5) + 0), y: Math.floor((Math.random() * 100) + 0), transformOrigin:"0% 50% -50",  delay: Math.floor((Math.random() * 3) + 0),ease: Elastic.easeOut}, Math.random());
       })
     }
 
-    animateText();
+    tl.staggerFrom(".boxAnim", 1, {opacity: 0, scale: 0, delay: 1, ease: Elastic.easeOut}, 0.1);
 
-    var tl = new TimelineMax();
-
-    TweenMax.from(".napFace", 1, {opacity: 0, scale: 0, delay: 0.5, ease: Elastic.easeOut});
-
-    TweenMax.staggerFrom(".boxAnim", 1, {opacity: 0, scale: 0, delay: 1, ease: Elastic.easeOut}, 0.1);
-
-    TweenMax.to(".napFace", 0.5, {opacity: 0, delay: 2, ease: Elastic.easeOut, onComplete: complete});
+    tl.from(".napFace", 2, {opacity: 0, delay: 2, repeat:10, onRepeat:onRepeat, repeatDelay:2, ease: RoughEase.ease.config({ template: Power0.easeNone, strength: 1.5, points: 20, taper: "none", randomize: true, clamp: false})});
 
     function complete(){
-      TweenMax.to(".napFace", 0.4, {opacity: 1, delay: 0.1, ease: Elastic.easeOut});}
+      tl.to(".napFace", 0.4, {opacity: 1, delay: 0.1, ease: Elastic.easeOut});}
 
     function rotateFace(value){
-      console.log('por lo menos lo hace')
-      TweenMax.to(".napFace", 0.5, {rotation: value, ease: Elastic.easeOut});
+      tl.to(".napFace", 0.5, {rotation: value, ease: Elastic.easeOut});
     }
 
-    TweenMax.to(".napFace", 0.5, {opacity: 0, repeatDelay: 8, ease: Elastic.easeOut, onComplete: complete});
+    function onRepeat() {
+    }
 
-    /*setInterval(function(){
-      animateText();
-      TweenMax.to(".napFace", 0.5, {opacity: 0, delay: 2, ease: Elastic.easeOut, onComplete: complete});
-    }, 8000);*/
+    animateText();
 
     var items = $('.boxAnim');
 
