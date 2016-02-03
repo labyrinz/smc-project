6825d1b8a4092dc90903e38043090240b88087c5
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-02-03 18:09:46 +0100

    clean project

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,10 +12,11 @@ angular.module('smcApp')
       TweenMax.set(".videoClass, .scrollIcon, .hiddenCanvas", {visibility:"visible"})
       var tl = new TimelineMax();
 
-      tl.to(".videoClass", 1, {volume: 0.01, ease: Power2.easeOut})
-      tl.to(".BackVideo", 1, {opacity: 0.6, scale: 0.1, transformOrigin:"0.5% 2%", ease: Back.easeIn}, "-=0.5","video")
-      tl.to(".footer", 0.5,{bottom: 0, ease: Back.easeOut},"-=0.20","footer")
-      tl.staggerTo(".boxAnim", 0.2, {opacity: 1, scale: 1, ease: Elastic.easeOut, onComplete:animeButtons}, 0.1)
+      tl.to(".BackVideo", 1, {opacity: 0.6, scale: 0.1, transformOrigin:"0.5% 2%", ease: Back.easeIn})
+        .to(".videoClass", 1.5, {volume: 0.005, ease: Power2.easeOut},"-=0.7")
+        .to(".footer", 0.5,{bottom: 0, ease: Back.easeOut}, "footer")
+        .staggerTo(".boxAnim", 0.2, {opacity: 1, scale: 1, ease: Elastic.easeOut, onComplete:animeButtons}, 0.1);
+
       tl.pause();
 
     $(window).bind('mousewheel DOMMouseScroll mousedown', function(event){
@@ -34,8 +35,8 @@ angular.module('smcApp')
       });
 
     $(".videoClass").bind("ended", function() {
-      console.log("entra aqui??")
       TweenMax.to(".BackVideo", 3, {opacity:0, volume: 0, ease: Power2.easeOut, onComplete:hideElement});
+      tl.play("footer");
     });
 
     function hideElement(){ $(".BackVideo").css("display", "none") };
@@ -43,7 +44,7 @@ angular.module('smcApp')
     function animeButtons() {
       var items = $('.boxAnim');
       function over(){
-        TweenMax.to(this, 0.2, {y:-10,opacity:1}, 0.1)
+        TweenMax.to(this, 0.2, {y:-5,opacity:1}, 0.1)
         rotateFace(40);
       }
       function out(){
