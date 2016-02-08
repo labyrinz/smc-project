8719b0f71e45bc30cc79d0d54244b2078236a340
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-02-08 16:15:18 +0100

    var

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,14 +9,29 @@
  */
 angular.module('smcApp')
   .controller('MainCtrl', function ($scope) {
+      var soundEpilogo = new Howl({
+        urls: ['audio/ValsViudaAlegre.mp3'],
+        loop: true,
+        volume: 0.0001,
+        onend: function() {
+          console.log('Finished!');
+        }
+      })
+
+      var step = 0;
       TweenMax.set(".videoClass, .scrollIcon, .hiddenCanvas", {visibility:"visible"})
-      var tl = new TimelineMax();
+      var tl = new TimelineMax({repeat:0});
 
-      tl.to(".BackVideo", 1, {opacity: 0.6, scale: 0.1, transformOrigin:"0.5% 2%", ease: Back.easeIn})
+      tl.to(".BackVideo", 1, {opacity: 0.6, scale: 0.1, transformOrigin:"0.5% 2%", ease: Back.easeIn, onComplete: playMusic}, "miniVideo")
         .to(".videoClass", 1.5, {volume: 0.005, ease: Power2.easeOut},"-=0.7")
-        .to(".footer", 0.5,{bottom: 0, ease: Back.easeOut}, "footer")
-        .staggerTo(".boxAnim", 0.2, {opacity: 1, scale: 1, ease: Elastic.easeOut, onComplete:animeButtons}, 0.1);
-
+        .to(soundEpilogo, 3,{volume: 0.05, ease: Power0.easeNone},"miniVideo")
+        .to(".footer", 0.5,{bottom: 0, ease: Back.easeOut}, "0.2+=miniVideo")
+        .to(".BackVideo", 3, {opacity:0, volume: 0, ease: Power2.easeOut})
+        //.to(".napFace", 3, {right:'0%', ease: Power2.easeOut}, "-=3")
+        .to(".fase3", 3.04, {right:'0%', ease: Back.easeOut})
+        .to(".fase2", 3.02, {right:'0%', ease: Back.easeOut}, "-=3")
+        .to(".fase1", 3, {right:'0%', ease: Back.easeOut}, "-=3")
+        .staggerTo(".boxAnim", 0.2, {opacity: 1, scale: 1, ease: Elastic.easeOut, onComplete: animeButtons}, 0.1, "-=3");
       tl.pause();
 
     $(window).bind('mousewheel DOMMouseScroll mousedown', function(event){
@@ -24,22 +39,30 @@ angular.module('smcApp')
         if(event.type != 'mousedown'){
           if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
             tl.reverse();
+            setTimeout(pauseAnim, 300);
           }
           else {
             tl.play();
+            setTimeout(pauseAnim, 300);
           }
         }
         else if(event.type == 'mousedown' && event.button == 1) {
-
+          tl.play();
         }
       });
 
     $(".videoClass").bind("ended", function() {
-      TweenMax.to(".BackVideo", 3, {opacity:0, volume: 0, ease: Power2.easeOut, onComplete:hideElement});
-      tl.play("footer");
+      tl.play();
     });
 
-    function hideElement(){ $(".BackVideo").css("display", "none") };
+    function playMusic(){
+      console.log(soundEpilogo);
+      //soundEpilogo.play();
+    }
+
+    function pauseAnim(){
+      tl.pause();
+    }
 
     function animeButtons() {
       var items = $('.boxAnim');
