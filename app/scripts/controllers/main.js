2ec3ea8a7e27a812be291814bf0d5d059a37c8ca
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-02-10 09:31:28 +0100

    pentagram

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,19 +22,19 @@ angular.module('smcApp')
       var wordsElement = $(".word-measure");
 
       var step = 0;
-      TweenMax.set(".videoClass, .scrollIcon, .hiddenCanvas", {visibility:"visible"})
+      TweenMax.set(".videoClass, .scrollIcon, .hiddenCanvas, .dinamycText", {visibility:"visible"})
       var tl = new TimelineMax({repeat:0});
 
       tl.to(".BackVideo", 1, {opacity: 0.6, scale: 0.1, transformOrigin:"0.5% 2%", ease: Back.easeIn, onComplete: playMusic}, "miniVideo")
         .to(".videoClass", 1.5, {volume: 0.005, ease: Power2.easeOut},"-=0.7")
-        .to(soundEpilogo, 3,{volume: 0.05, ease: Power0.easeNone},"miniVideo")
-        .to(".footer", 0.5,{bottom: 0, ease: Back.easeOut}, "0.2+=miniVideo")
-        .to(".BackVideo", 3, {opacity:0, volume: 0, ease: Power2.easeOut})
-        .staggerTo(".boxAnim", 0.2, {opacity: 1, scale: 1, ease: Elastic.easeOut, onComplete: animeButtons}, 0.1, "-=3")
+        //.to(soundEpilogo, 3,{volume: 0.05, ease: Power0.easeNone},"miniVideo")
+        //.to(".footer", 0.5,{bottom: 0, ease: Back.easeOut}, "0.2+=miniVideo")
+        //.to(".BackVideo", 3, {opacity:0, volume: 0, ease: Power2.easeOut})
+        //.staggerTo(".boxAnim", 0.2, {opacity: 1, scale: 1, ease: Elastic.easeOut, onComplete: animeButtons}, 0.1, "-=3")
         //.to(".fase3", 3.06, {right:'0%', ease: Back.easeOut})
         //.to(".fase2", 3.03, {right:'0%', ease: Back.easeOut}, "-=3")
         //.to(".fase1", 3, {right:'0%', ease: Back.easeOut}, "-=3")
-        .to(".napFace", 3, {right:'0%', ease: Power2.easeOut, onComplete: animateText}, "-=3");
+        //.to(".napFace", 3, {right:'0%', ease: Power2.easeOut, onComplete: animateText}, "-=3");
       tl.pause();
 
     $(window).bind('mousewheel DOMMouseScroll mousedown', function(event){
