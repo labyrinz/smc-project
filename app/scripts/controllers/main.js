21d400cc3fb8df638ce4b5c66cb858431b29f231
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-02-25 18:34:34 +0100

    m

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,111 +10,141 @@
 angular.module('smcApp')
   .controller('MainCtrl', function ($scope) {
 
-      var camera, scene, renderer, car, car2,
+      //--- GLOBAL VARIABLES ----
+
+      var camera, scene, renderer, car, directionalLight, velocity,
         width = window.innerWidth,
         height = window.innerHeight;
+      var carMovOrient = 0;
 
-      var soundEpilogo = new Howl({
-        urls: ['audio/ValsViudaAlegre.mp3'],
-        loop: true,
-        volume: 0.0001,
-        onend: function() {
-          console.log('Finished!');
-        }
-      })
-      var aspectRatio = (width/1920);
-      console.log(aspectRatio);
       var body = $('body');
-      var backColorArray = ['#635957','#F7B862','#6B4B30','#63431D','#C48332','#F7B862','#6B4B30'];
 
       var mySplitText = $("#quote").splitText({'type':'words','animation':'glowOnHover','useLite':true});
       var wordsElement = $(".word-measure");
 
       var step = 0;
-      TweenMax.set(".scrollIcon, .hiddenCanvas, .dinamycText", {visibility:"visible"})
-      var tl = new TimelineMax({repeat:0});
 
-      tl
-        //.to(soundEpilogo, 3,{volume: 0.05, ease: Power0.easeNone},"miniVideo")
-        .to(".back", 14, {left:'-690%', ease: Power0.easeNone}, "penta");
-        //.to(".pentagram2", 3, {left:'-9090px', ease: Power0.easeNone}, "penta")
-      tl.pause();
+      //-----------------------
+      //----SOUND TRACKS -----
 
-    $(window).bind('mousewheel DOMMouseScroll', function(event){
-        event.preventDefault();
-        if(event.type != 'mousedown'){
-          if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-            if(step>0){
-              if(step>0.005)step -= 0.005;
-              else if(step<=0.005 )step = 0;
-              TweenLite.to(tl, 0.5, {progress:step, ease:Power2.easeOut, onComplete: pauseAnim});
-            }
+        var soundEpilogo = new Howl({
+          urls: ['audio/ValsViudaAlegre.mp3'],
+          loop: false,
+          volume: 0.0001,
+          onend: function() {
+            console.log('Finished!');
           }
-          else {
-            if(step<1){
-              TweenLite.to(tl, 0.5, {progress:step, ease:Power2.easeOut, onComplete: pauseAnim});
-              step += 0.005;
-              //$("input[type=range]").val((step/2.5)*26.5);
+        });
+
+      //-----------------------
+      //---------VIDEOS--------
+
+        $(".videoClass").bind("ended", function() {
+          TweenMax.to(".BackVideo", 4, {opacity: 0, ease: Back.easeIn, onComplete: playMusic}, "miniVideo")
+          TweenMax.to(".videoClass", 1.5, {volume: 0, ease: Power2.easeOut})
+        });
+
+      //-----------------------
+      //-----TIMELINE ---------
+
+        TweenMax.set(".scrollIcon, .hiddenCanvas, .dinamycText", {visibility:"visible"})
+
+        var tl = new TimelineMax({repeat:0});
+
+        tl.to(".back", 14, {left:'-690%', ease: Power0.easeNone}, "penta");
+        tl.pause();
+
+      //---------------------------------
+      //----------MOUSE CONTROLS --------
+
+        $(window).bind('mousewheel DOMMouseScroll', function(event){
+            event.preventDefault();
+            if(event.type != 'mousedown'){
+              if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
+                if(step>0){
+                  if(step>0.005)step -= 0.005;
+                  else if(step<=0.005 )step = 0;
+                  TweenLite.to(tl, 0.5, {progress:step, ease:Power2.easeOut, onComplete: pauseAnim});
+                }
+              }
+              else {
+                if(step<1){
+                  TweenLite.to(tl, 0.5, {progress:step, ease:Power2.easeOut, onComplete: pauseAnim});
+                  step += 0.005;
+                  //$("input[type=range]").val((step/2.5)*26.5);
+                }
+              }
             }
-          }
+          });
+
+      //------------------------------------
+      //------SLIDE CONTROL ---------------
+
+        $("#slider").on("input", function(){
+
+          velocity = Math.abs(this.value);
+
+          tl.timeScale(velocity/2);
+
+          car.position.x = -35 + (this.value*3);
+
+          if(this.value>=0){ tl.play(); carMovOrient = 1; }
+          else { tl.reverse(); carMovOrient = -1; }
+
+        });
+
+        $("#slider").on("mouseup", function(){
+            this.value = 0;
+            carMovOrient = 0;
+            car.position.x = -35;
+            tl.timeScale(0);
+            tl.pause();
+          });
+
+      //--------------------------------------
+      //------TEXTS --------------------------
+
+      function animateText(){
+          $(".dinamycText").css("opacity", "1");
+          _.each(wordsElement, function(element){
+            TweenMax.from(element, 3, {
+                opacity:0,
+                scale:Math.floor((Math.random() * 5) + 0),
+                y: Math.floor((Math.random() * 200) + 0),
+                x: Math.floor((Math.random() * 200) + 0),
+                transformOrigin:"0% 50% -50",
+                delay: Math.floor((Math.random() * 3) + 0),
+                ease: Power2.easeOut},
+              Math.random());
+          })
         }
-      });
 
-    $(".videoClass").bind("ended", function() {
-      TweenMax.to(".BackVideo", 4, {opacity: 0, ease: Back.easeIn, onComplete: playMusic}, "miniVideo")
-      TweenMax.to(".videoClass", 1.5, {volume: 0, ease: Power2.easeOut})
-    });
+      //-------------------------------------
+      //-------FUNCTIONS --------------------
 
-    $("#slider").on("input", function(){
-      var velocity = Math.abs(this.value)
-      tl.timeScale(velocity/2);
-      car.position.x = -35 + (this.value*3);
-      console.log(car.position.x);
-      if(this.value>=0)tl.play();
-      else tl.reverse();
-    });
-    $("#slider").on("mouseup", function(){
-      this.value = 0;
-      car.position.x = -35;
-      tl.timeScale(0);
-      tl.pause();
-    });
+        function playMusic(){
+            $(".BackVideo").css("display","none");
+            soundEpilogo.play();
+            TweenMax.to(soundEpilogo, 20,{volume: 0.5, ease: Power0.easeNone})
+          }
 
-    function animateText(){
-      $(".dinamycText").css("opacity", "1");
-      _.each(wordsElement, function(element){
-        TweenMax.from(element, 3, {
-            opacity:0,
-            scale:Math.floor((Math.random() * 5) + 0),
-            y: Math.floor((Math.random() * 200) + 0),
-            x: Math.floor((Math.random() * 200) + 0),
-            transformOrigin:"0% 50% -50",
-            delay: Math.floor((Math.random() * 3) + 0),
-            ease: Power2.easeOut},
-          Math.random());
-      })
-    }
-    function playMusic(){
-      console.log(soundEpilogo);
-      $(".BackVideo").css("display","none");
-      //soundEpilogo.play();
-    }
+        function pauseAnim(){
+            tl.pause();
+          }
 
-    function pauseAnim(){
-      tl.pause();
-    }
+        function animeButtons() {
+            var items = $('.boxAnim');
+            function over(){
+              TweenMax.to(this, 0.2, {y:-5,opacity:1}, 0.1)
+            }
+          function out(){
+              TweenMax.to(this, 0.2, {y:0,  opacity:1}, 0.1)
+            }
+            items.hover(over, out);
+        }
 
-    function animeButtons() {
-      var items = $('.boxAnim');
-      function over(){
-        TweenMax.to(this, 0.2, {y:-5,opacity:1}, 0.1)
-      }
-      function out(){
-        TweenMax.to(this, 0.2, {y:0,  opacity:1}, 0.1)
-      }
-      items.hover(over, out);
-    }
-    /////----------WEBGL-----------------
+      //-----------------------------------
+      //----------WEBGL--------------------
 
       init();
       animate();
@@ -136,9 +166,9 @@ angular.module('smcApp')
 
         buildShape();
 
-        var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
-        directionalLight.position.set(20,35,25);
-        directionalLight.name = 'luzDireccional'
+        directionalLight = new THREE.PointLight( 0xffffff, 1, 300 );
+        directionalLight.position.set(0,5,60);
+        directionalLight.name = 'luzDireccional';
 
         scene.add( directionalLight );
 
@@ -153,7 +183,7 @@ angular.module('smcApp')
             if ( child instanceof THREE.Mesh ) {
               if(child.name == "CorvegaConvBody") {
                 child.material.ambient.setHex(0xFFFFFF);
-                child.material.color.setHex(0xCCCCCC);
+                child.material.color.setHex(0xFFFFFF);
               }
               if(child.name == "CorvegaConvGlass") {
                 child.material.ambient.setHex(0xFFFFFF);
@@ -179,6 +209,20 @@ angular.module('smcApp')
         renderer.setSize( window.innerWidth, window.innerHeight*0.1 );
       }
      function animate() {
+
+       if(carMovOrient > 0){
+         if(directionalLight.position.x < -300) directionalLight.position.x = 300;
+         else directionalLight.position.x = directionalLight.position.x - velocity*10;
+       }
+       else if(carMovOrient < 0) {
+         if(directionalLight.position.x > 300) directionalLight.position.x = -300;
+         else directionalLight.position.x = directionalLight.position.x + velocity*10;
+       }
+       else if(carMovOrient == 0) {
+         directionalLight.position.x = 5;
+       }
+
+
         setTimeout( function() {
           requestAnimationFrame( animate );
         }, 1000/30 );
