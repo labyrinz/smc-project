d27e992c63de944afad8a84c0245d06dffe23a0c
beeva-sergiosantamaria <sergio.santamaria@beeva.com>
2016-04-27 23:49:36 +0200

    drive videos

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,13 +12,10 @@ angular.module('smcApp')
 
       //--- GLOBAL VARIABLES ----
 
-      var velocity, width = window.innerWidth, height = window.innerHeight;
       var mapStatus = false;
       var videoDisplay = true;
       var body = $('body');
-      var stepIncrement = 0.001;
       var totalWords = [];
-      var player;
 
       var introLetters = $("#quote").splitText({'type':'words','animation':'glowOnHover','useLite':true,'addClass':"introLetters"});
       var introWords = $(".introLetters");
@@ -71,26 +68,15 @@ angular.module('smcApp')
         //    quitVideo();
         //});
 
-        function quitVideo(){
-          if(videoDisplay){
-              soundEpilogo.play();
-              tl.play();
-              videoDisplay = false;
-            }
-          }
+        //function quitVideo(){
+        //  if(videoDisplay){
+        //      soundEpilogo.play();
+        //      tl.play();
+        //      videoDisplay = false;
+        //    }
+        //  }
       //-----------------------
       //-----TIMELINE ---------
-
-        var arr1 = [0, 0, 60, 0, 35, 100, 0, 100];
-        var arr2 = [90, 0, 90, 0, 90, 100, 90, 100];
-        var arr3 = [56, 0, 90, 0, 90, 100, 43, 100];
-
-        TweenMax.to('.openBook', 0.5, {right: '4%', repeatDelay:0.1, repeat:-1, yoyo:true, ease: Power2.easeOut});
-
-        arr2.onUpdate = function() {
-          TweenMax.set('.prel01',{webkitClipPath:'polygon('+arr1[0]+'%'+arr1[1]+'%,'+arr1[2]+'%'+arr1[3]+'%,'+arr1[4]+'%'+arr1[5]+'%,'+arr1[6]+'%'+arr1[7]+'%)'});
-          TweenMax.set('.prelGroup',{webkitClipPath:'polygon('+arr3[0]+'%'+arr3[1]+'%,'+arr3[2]+'%'+arr3[3]+'%,'+arr3[4]+'%'+arr3[5]+'%,'+arr3[6]+'%'+arr3[7]+'%)'});
-        };
 
         TweenMax.set(".scrollIcon, .hiddenCanvas, .dinamycText, .ageTitle, .napFace, .addon1, .prel01", {visibility:"visible"});
 
@@ -98,20 +84,17 @@ angular.module('smcApp')
 
         tl
           //EPISODE 1
-          .to("#page0",0.4,{ right: '0%', ease: Power0.easeNone})
           .addPause()
           .staggerFrom(introWords, 0.1, {opacity: 0, cycle:{scale:[0,5], y:[-50,200], x:[-50,200], transformOrigin:"0% 50% -50", delay:[0,0.2]}, ease: Back.easeOut.config(0.8)}, 0.1)
           .addPause()
           //EPISODE 2
-
-          //.to(".age1", 0.5, {color:'#ffd85f', fontSize: '2em', opacity: 1,  ease:Power0.easeNone})
-          //.to("#page0",0.5,{ scale: '0', onComplete: hideVideo, ease: Back.easeIn.config(1)})
           .to(".ed1", 0.5, {left: '0%', ease: Bounce.easeOut})
           .to(".ed2", 0.5, {top: '0%', ease: Bounce.easeOut})
           .to(".ed3", 0.5, {left: '0%', ease: Bounce.easeOut},"-=1")
           .to(".ed4", 0.5, {transform: 'rotateX(0deg)', ease: Back.easeOut.config(1)})
           .to(".pentagramRect",0.2,{ bottom: '1%', ease: Power0.easeNone},"-=0.2")
           .to(".pentagramBack",0.2,{ bottom: '0%', ease: Power0.easeNone},"-=0.2")
+          .to(".pentagramNotesGroup",0.2,{ bottom: '2%', ease: Power0.easeNone},"-=0.2")
           .to("#page1",0.1,{ right: '0%', onComplete: hideVideo, ease: Power0.easeNone},"-=0.5")
           .to(".texto11", 0.5, {top: '9%', ease: Back.easeOut.config(1)})
           .to(".texto12", 0.5, {top: '30%', ease: Back.easeOut.config(1)},"-=0.2")
@@ -121,8 +104,6 @@ angular.module('smcApp')
           .to(".cub1", 0.5, {top: '0%', ease: Power0.easeNone})
           .to("#page0",0.5,{ scale: '0', ease: Back.easeIn.config(1)})
           .addPause()
-          //.to(arr1,1, arr2,"step3")
-          //.to(arr3,1, arr2,"step3")
           //EPISODE 3
           .to(".ed2", 0.5, {left: '-5%', ease: Power2.easeIn},"scrollGer")
           .to(".ed3", 0.5, {left: '-10%', ease: Power2.easeIn},"scrollGer")
@@ -148,7 +129,7 @@ angular.module('smcApp')
           .to(".texto16",0.4,{ right: '10%', ease: Back.easeOut.config(1)},"-=0.2")
           .addPause()
           //EPISODE 5
-          .to(".cub1", 0.5, {transform: 'rotateY(150deg)', ease: Power2.easeIn})
+          .to(".cub1", 0.5, {transform: 'rotateY(165deg)', ease: Power2.easeIn})
           .to("#page3",0.4,{ right: '100%', ease: Power0.easeNone},"-=0.4")
           .to("#page4",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
           .to(".ny4", 0.2, {opacity: '1', ease: Power2.easeIn},"-=1")
@@ -160,8 +141,6 @@ angular.module('smcApp')
           .to(".cita12",0.4,{ right: '20%', ease: Back.easeOut.config(1)},"-=0.2")
           .addPause()
           //EPISODE 6
-          //.to(".age2", 0.5, {color:'#ffd85f', fontSize: '2em', opacity: 1, ease:Power0.easeNone})
-          //.to(".back3",0.4,{ opacity: '0', ease: Power0.easeNone})
           .to("#page4",0.4,{ right: '100%', ease: Power0.easeNone})
           .to("#page5",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
           .to(".rm12",0.4,{ right: '70%', ease: Back.easeOut.config(1)})
@@ -173,10 +152,10 @@ angular.module('smcApp')
           //EPISODE 7
           .to("#page5",0.4,{ right: '100%', ease: Power0.easeNone})
           .to("#page6",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
-          .to(".ny3", 0.3, {transform: 'rotateY(150deg)', ease: Power2.easeIn})
-          .to(".ny1", 0.3, {transform: 'rotateY(150deg)', ease: Power2.easeIn})
-          .to(".ny2", 0.3, {transform: 'rotateY(150deg)', ease: Power2.easeIn})
-          .to(".ny4", 0.3, {transform: 'rotateY(150deg)', ease: Power2.easeIn})
+          .to(".ny3", 0.3, {transform: 'rotateY(165deg)', ease: Power2.easeIn})
+          .to(".ny1", 0.3, {transform: 'rotateY(165deg)', ease: Power2.easeIn})
+          .to(".ny2", 0.3, {transform: 'rotateY(165deg)', ease: Power2.easeIn})
+          .to(".ny4", 0.3, {transform: 'rotateY(165deg)', ease: Power2.easeIn})
           .to(".ber1", 0.3, {transform: 'rotateY(0deg)', ease: Back.easeOut.config(1)})
           .to(".texto23",0.4,{ right: '60%', ease: Back.easeOut.config(1)})
           .to(".rm14",0.4,{ right: '60%', ease: Back.easeOut.config(1)},"-=0.2")
@@ -186,7 +165,7 @@ angular.module('smcApp')
           //EPISODE 8
           .to("#page6",0.4,{ right: '100%', ease: Power0.easeNone})
           .to("#page7",0.2,{ right: '0%', ease: Power0.easeNone},"-=0.4")
-          .to(".ber1", 0.3, {transform: 'rotateY(150deg)', ease: Back.easeOut.config(1)})
+          .to(".ber1", 0.3, {transform: 'rotateY(165deg)', ease: Back.easeOut.config(1)})
           .to(".holly1", 0.3, {transform: 'rotateX(0deg)', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".texto25",0.4,{ right: '10%', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".rm15",0.4,{ right: '80%', ease: Back.easeOut.config(1)},"-=0.2")
@@ -196,10 +175,9 @@ angular.module('smcApp')
           .to(".rm18",0.4,{ right: '70%', ease: Back.easeOut.config(1)},"-=0.2")
           .addPause()
           //EPISODE 9
-          //.to(".age3", 0.5, {color:'#ffd85f', fontSize: '2em', opacity: 1, ease:Power0.easeNone})
           .to("#page7",0.4,{ right: '100%', ease: Power0.easeNone})
           .to("#page8",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
-          .to(".holly1", 0.3, {transform: 'rotateX(150deg)', ease: Back.easeOut.config(1)})
+          .to(".holly1", 0.3, {transform: 'rotateX(165deg)', ease: Back.easeOut.config(1)})
           .to(".carn1", 0.3, {transform: 'rotateY(0deg)', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".texto31",0.4,{ right: '30%', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".cc31",0.4,{ right: '60%', ease: Back.easeOut.config(1)},"-=0.2")
@@ -223,9 +201,8 @@ angular.module('smcApp')
           .to("#page10",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
           .addPause()
           //EPISODE 12
-          //.to(".age4", 0.5, {color:'#ffd85f', fontSize: '2em', opacity: 1, ease:Power0.easeNone})
           .to("#page10",0.4,{ right: '100%', ease: Power0.easeNone})
-          .to(".carn1", 0.3, {transform: 'rotateY(150deg)', ease: Back.easeOut.config(1)},"-=0.2")
+          .to(".carn1", 0.3, {transform: 'rotateY(165deg)', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".chi1", 0.3, {transform: 'rotateY(0deg)', ease: Back.easeOut.config(1)},"-=0.2")
           .to("#page11",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
           .to(".lorraine",0.4,{ right: '35%', ease: Back.easeOut.config(1)},"-=0.2")
@@ -250,10 +227,9 @@ angular.module('smcApp')
           .to(".la9",0.4,{ bottom: '60%', ease: Back.easeOut.config(1)},"-=0.2")
           .addPause()
           //EPISODE 15
-          //.to(".age5", 0.5, {color:'#ffd85f', fontSize: '2em', opacity: 1, ease:Power0.easeNone})
           .to("#page13",0.4,{ right: '100%', ease: Power0.easeNone})
           .to("#page14",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
-          .to(".chi1", 0.3, {transform: 'rotateY(150deg)', ease: Back.easeOut.config(1)},"-=0.2")
+          .to(".chi1", 0.3, {transform: 'rotateY(165deg)', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".lasv1", 0.3, {transform: 'rotateY(0deg)', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".texto52",0.4,{ right: '10%', ease: Back.easeOut.config(1)},"-=0.2")
           .to(".cita53",0.4,{ right: '40%', ease: Back.easeOut.config(1)},"-=0.2")
@@ -287,6 +263,10 @@ angular.module('smcApp')
           //EPISODE 18
           .to("#page16",0.4,{ right: '100%', ease: Power0.easeNone})
           .to("#page17",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
+          .addPause()
+          //EPISODE 19
+          .to("#page17",0.4,{ right: '100%', ease: Power0.easeNone})
+          .to("#page18",0.4,{ right: '0%', ease: Power0.easeNone},"-=0.4")
           .addPause();
 
        tl.play();
@@ -301,24 +281,9 @@ angular.module('smcApp')
             TweenMax.to('.additional', 0.2, {opacity: 0, scale:0, ease:Back.easeOut});
             if(event.type != 'mousedown'){
               if (event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0) {
-                //if(step > 0){
-                //  velocity = 1;
-                //  //if(!mapStatus && step < 0.1){openCloseMap();}
-                //  if(step > stepIncrement)step -= stepIncrement;
-                //  else if(step <= stepIncrement ) step = 0;
-                //  TweenLite.to(tl, 0.5, {progress:step, ease:Power2.easeOut, onComplete: pauseAnim});
-                //  //$("#bookPages").turn("previous");
-                //}
                 tl.reverse();
               }
               else {
-                //if(step < 1.2){
-                //  //if(mapStatus) openCloseMap();
-                //  velocity = 1;
-                //  TweenLite.to(tl, 0.5, {progress:step, ease:Power2.easeOut, onComplete: pauseAnim});
-                //  step += stepIncrement;
-                //  //$("#bookPages").turn("next");
-                //}
                 tl.play();
               }
             }
@@ -327,84 +292,84 @@ angular.module('smcApp')
     //------------------------------------
     //---------MAP D3 CONTROLLER -----------
 
-      var width = window.innerWidth/1.5,
-          height = window.innerHeight/2;
-      var circulitos;
-
-      if(width<1500) var dimensions = {leftCenter:-40,upCenter: 20,scaleX:300, font: "14px"};
-      else var dimensions = {leftCenter:-80,scaleX:400, font: "22px"};
-
-      var projection = d3.geo.mercator()
-        .center([dimensions.leftCenter, dimensions.upCenter])
-        .scale(dimensions.scaleX)
-        .rotate([0,0]);
-
-      var svg = d3.select("#mapContainer").append("svg")
-        .attr("width", width)
-        .attr("height", height);
-
-      var path = d3.geo.path()
-        .projection(projection);
-
-      var g = svg.append("g");
-
-      d3.json("images/models/world-110m2.json", function(error, topology) {
-        d3.csv("images/models/cities.csv", function(error, data) {
-        var elementy1 = g.selectAll("circle")
-            .data(data)
-            .enter()
-          circulitos = elementy1.append("circle")
-              .attr("cx", function(d) {
-                return projection([d.lon, d.lat])[0];
-              })
-              .attr("cy", function(d) {
-                return projection([d.lon, d.lat])[1];
-              })
-              .attr("r", 7)
-              .attr("id", 'circulos')
-              .style("opacity", "0.7")
-              .style("fill", "rgba(255,255,255,0.2)")
-              .style("stroke", "#ffd85f")
-              .style("stroke-width", "2")
-              .style("cursor", "pointer");
-          elementy1.append('text')
-              .text(function(d) {
-                return d.name;
-              })
-              .attr('x', function(d) {
-                return (projection([d.lon, d.lat])[0])+ (d.precisionX*1);
-              })
-              .attr('y', function(d) {
-                return (projection([d.lon, d.lat])[1])+ (d.precisionY*1);
-              })
-              .attr('fill', '#ffd85f')
-              .style("font-size", dimensions.font)
-              .style("font-family", "'Alice',serif")
-              .style("cursor", "pointer");
-        });
-        g.selectAll("path")
-          .data(
-          topojson
-            .object(topology, topology.objects.countries)
-            .geometries)
-          .enter()
-          .append("path")
-          .attr("d", path)
-          .attr('stroke', '#ffd85f')
-          .attr('stroke-width', '0.5px')
-          .attr('fill', '#56481e')
-          .on('mouseover', function(d) {
-            console.log(d);
-          })
-      });
-
-      setInterval(function(){
-          circulitos.attr("r",10);
-          circulitos
-            .transition()
-            .duration(800)
-            .attr("r",1);
-        }, 1000);
+      //var width = window.innerWidth/1.5,
+      //    height = window.innerHeight/2;
+      //var circulitos;
+      //
+      //if(width<1500) var dimensions = {leftCenter:-40,upCenter: 20,scaleX:300, font: "14px"};
+      //else var dimensions = {leftCenter:-80,scaleX:400, font: "22px"};
+      //
+      //var projection = d3.geo.mercator()
+      //  .center([dimensions.leftCenter, dimensions.upCenter])
+      //  .scale(dimensions.scaleX)
+      //  .rotate([0,0]);
+      //
+      //var svg = d3.select("#mapContainer").append("svg")
+      //  .attr("width", width)
+      //  .attr("height", height);
+      //
+      //var path = d3.geo.path()
+      //  .projection(projection);
+      //
+      //var g = svg.append("g");
+      //
+      //d3.json("images/models/world-110m2.json", function(error, topology) {
+      //  d3.csv("images/models/cities.csv", function(error, data) {
+      //  var elementy1 = g.selectAll("circle")
+      //      .data(data)
+      //      .enter()
+      //    circulitos = elementy1.append("circle")
+      //        .attr("cx", function(d) {
+      //          return projection([d.lon, d.lat])[0];
+      //        })
+      //        .attr("cy", function(d) {
+      //          return projection([d.lon, d.lat])[1];
+      //        })
+      //        .attr("r", 7)
+      //        .attr("id", 'circulos')
+      //        .style("opacity", "0.7")
+      //        .style("fill", "rgba(255,255,255,0.2)")
+      //        .style("stroke", "#ffd85f")
+      //        .style("stroke-width", "2")
+      //        .style("cursor", "pointer");
+      //    elementy1.append('text')
+      //        .text(function(d) {
+      //          return d.name;
+      //        })
+      //        .attr('x', function(d) {
+      //          return (projection([d.lon, d.lat])[0])+ (d.precisionX*1);
+      //        })
+      //        .attr('y', function(d) {
+      //          return (projection([d.lon, d.lat])[1])+ (d.precisionY*1);
+      //        })
+      //        .attr('fill', '#ffd85f')
+      //        .style("font-size", dimensions.font)
+      //        .style("font-family", "'Alice',serif")
+      //        .style("cursor", "pointer");
+      //  });
+      //  g.selectAll("path")
+      //    .data(
+      //    topojson
+      //      .object(topology, topology.objects.countries)
+      //      .geometries)
+      //    .enter()
+      //    .append("path")
+      //    .attr("d", path)
+      //    .attr('stroke', '#ffd85f')
+      //    .attr('stroke-width', '0.5px')
+      //    .attr('fill', '#56481e')
+      //    .on('mouseover', function(d) {
+      //      console.log(d);
+      //    })
+      //});
+      //
+      //setInterval(function(){
+      //    circulitos.attr("r",10);
+      //    circulitos
+      //      .transition()
+      //      .duration(800)
+      //      .attr("r",1);
+      //  }, 1000);
 
       //--------------------------------------
       //-------FUNCTIONS --------------------
@@ -419,14 +384,6 @@ angular.module('smcApp')
 
         $(document).on('click','#arrowPrev',function(){
           prevFoto();
-        });
-
-        $(document).on('click','#openPhoto',function(){
-          openPhoto();
-        });
-
-        $(document).on('click','#closePhoto',function(){
-          closePhoto();
         });
 
         $(document).on('click','.plusInfo',function(){
@@ -477,18 +434,6 @@ angular.module('smcApp')
           TweenMax.to(firstPhoto, 0.1, {left: '-130%', repeatDelay:0, repeat:1, yoyo:true, onRepeat:$('#fotoGroup').append(firstPhoto), ease: Power2.easeOut});
         }
 
-        function openPhoto(){
-          var firstPhoto = $('.slideimg').last();
-          $(firstPhoto).addClass('bigPhoto');
-          $(firstPhoto).css('left', '');
-          TweenMax.to(".closePhoto", 0.5, {scale: 1, delay: 0.5, ease: Back.easeOut});
-        }
-        function closePhoto(){
-          var firstPhoto = $('.slideimg').last();
-          $(firstPhoto).removeClass('bigPhoto');
-          TweenMax.to(".closePhoto", 0.5, {scale: 0, ease: Back.easeOut});
-        }
-
         function openCloseMap(val) {
           if(val==undefined){
             if(!mapStatus){
@@ -522,7 +467,6 @@ angular.module('smcApp')
           console.log($('#introVideo'));
           $('#introVideo').attr('src', '');
           $('#introVideo').attr('src', 'https://www.youtube.com/embed/asF4OUyq7Y8');
-          //$(".BackVideo").css("display","none");
         }
 
         function initViaje(){
